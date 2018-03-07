5bd0f0f1a23678cc73232da2d44daf1f0ee651ab
Anson <leungans@gmail.com>
2018-03-06 21:20:17 -0800

    fizzbuzz

---
+++ algos/fizz_buzz.js
@@ -0,0 +1,17 @@
+// leetcode
+
+var fizzBuzz = function(n) {
+  var results = []
+    for (var i = 1; i <= n; i++) {
+        if (i % 5 == 0 && i % 3 == 0) {
+            results.push("FizzBuzz")
+        } else if (i % 5 == 0) {
+            results.push("Buzz")
+        } else if (i % 3 == 0) {
+            results.push("Fizz")
+        } else {
+            results.push(i.toString())
+        }
+    }
+    return results
+};
